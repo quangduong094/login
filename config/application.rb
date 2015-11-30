5c1c3f1c85a3c944a34a2d54f6fffaaac2ad5160
quangduong094 <quangduong0942@gmail.com>
2015-11-30 23:23:49 +0700

    Following

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module HomeworkWeek4LayoutWebsite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123151313) do
+ActiveRecord::Schema.define(version: 20151130155341) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
